8ef3d4d3e2a610ff0365ee4778715b13e3ce2825
Akanksha <akanksha.singh@doodleblue.com>
2018-08-14 17:24:12 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import reducers from './redux/reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import App from './App';
+
+
+let createStoreWithMiddleware = applyMiddleware(thunkMiddleware)(createStore)
+
+let store = createStoreWithMiddleware(reducers)
+
+
+ReactDOM.render(<Provider store={store} >
+    <App />
+</Provider>, document.getElementById('root'));
